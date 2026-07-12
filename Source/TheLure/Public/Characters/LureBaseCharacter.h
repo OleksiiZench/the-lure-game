bfa5a770a25ea7514020c362894d433ae21b4960
OleksiiZench <olekzenchenko@gmail.com>
2026-07-12 22:07:11 +0300

    feat(character): add BaseCharacter class

---
+++ Source/TheLure/Public/Characters/LureBaseCharacter.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+
+#include "LureBaseCharacter.generated.h"
+
+UCLASS(Abstract)
+class THELURE_API ALureBaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+	
+};
