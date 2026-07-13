0ce9139193e59306b4d6a92a8fc6315ac91be6c0
OleksiiZench <olekzenchenko@gmail.com>
2026-07-13 13:21:52 +0300

    feat(player): add LurePlayerController

---
+++ Source/TheLure/Public/Player/LurePlayerController.h
@@ -0,0 +1,18 @@
+// Copyright 2026 Oleksii Zenchenko & Denys Honcherenko. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+
+#include "LurePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THELURE_API ALurePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
