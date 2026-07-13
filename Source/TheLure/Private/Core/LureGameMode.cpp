70d19d5742aa40460d2a072ec7f7c07968ec3614
OleksiiZench <olekzenchenko@gmail.com>
2026-07-13 14:06:48 +0300

    feat(core): add LureGameState

--- Source/TheLure/Private/Core/LureGameMode.cpp
+++ Source/TheLure/Private/Core/LureGameMode.cpp
@@ -5,9 +5,11 @@
 
 #include "Player/LurePlayerCharacter.h"
 #include "Player/LurePlayerController.h"
+#include "Core/LureGameState.h"
 
 ALureGameMode::ALureGameMode()
 {
 	DefaultPawnClass = ALurePlayerCharacter::StaticClass();
 	PlayerControllerClass = ALurePlayerController::StaticClass();
+	GameStateClass = ALureGameState::StaticClass();
 }
